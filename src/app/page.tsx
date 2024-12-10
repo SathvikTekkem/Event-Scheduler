eec95a55ab51edf4ea13abead89e2712b3b4448d
SathvikTekkem <sathviktekkem@gmail.com>
2024-12-11 01:46:38 +0530

    Initial setup with basic structure

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div className="container mx-auto p-8">
+      <h1 className="text-4xl font-bold text-center mb-8">Event Scheduler</h1>
+      <p className="text-lg text-center mb-8">Welcome to your personal event scheduler. Please select a date to start scheduling your events!</p>
+
+      {/* Calendar and event scheduler components will go here */}
+    </div>
+  );
+}
